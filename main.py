089b0218fba7b38d1b878a3cb51316e376c1782e
Alex Belai <alex.belai112@gmail.com>
2020-11-08 12:16:58 +0100

    VAD Implementation (Yay! Speech almost done)

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import gpiozero # Library for pin inputs/outputs on the board
 from time import sleep # Delay function
 import sensor
 
+# TODO: Transfer to normal pbmm deepspeech model compatible with Pi
+# TODO: Implement Spinner to recognizing audio
+
+
 # Pin Definitions
 shutdown_button = gpiozero.Button(0, hold_time=2) # Have to hold it for 2 seconds to activate
 speech_button = gpiozero.Button(0)
@@ -18,16 +22,8 @@ motor1 = motors.Motor(0,0) # Left
 motor2 = motors.Motor(0,0) # Right
 motorGroup = [motor1, motor2]
 
-
-Sensor = sensor.UltrasonicSensor(0,1)
-
 # Initialize motor controller
 motorControl = motors.MotorController(motorGroup)
-
-# Create deepspeech model
-modelPath = str(Path().absolute()/'model'/'deepspeech-0.8.2-models.tflite') # TODO: Transfer to normal pbmm model compaible with Pi
-ds = deepspeech.Model(modelPath)
-
 
 def main():
     while True:
@@ -35,6 +31,15 @@ def main():
 
         # TODO: Reset pins at shutdown
 
+def init_speech_model():
+
+    # Initialize voice activity detector and audio stream with Aggressiveness = 0
+    vad_audio = speech.VoiceActivityDetector(0)
+    frames = vad_audio.vad_collector()
+
+    # Create deepspeech model
+    modelPath = str(Path().absolute()/'model'/'deepspeech-0.8.2-models.tflite')
+    ds = deepspeech.Model(modelPath)
 
 def shutdown():
     """Shut down system by passing a commmand to commmand line."""
--- speech.py
+++ speech.py
@@ -3,26 +3,34 @@ import numpy as np
 import pyaudio
 import time
 import webrtcvad
+import queue
+import collections
 
 # Global Variables
-FORMAT = pyaudio.paInt16 # 16-bit Format required by WebRTCVad
-INPUTRATE = 16000 # Sampling Rate = 16kHZ
-CHANNELS = 1 # Mono Audio required by WebRTCVad
+FORMAT = pyaudio.paInt16 # 16-bit format required by WebRTCVad
+INPUTRATE = 16000 # Sampling rate = 16kHZ
+CHANNELS = 1 # Mono audio required by WebRTCVad
+BLOCKS_PER_SECOND = 50 # 50 blocks of audio generated per second
 
-# TODO: Multithreading Implementation for Callback In Stream Init
-# TODO: Determine Resample function requirement? - probably no
+# TODO: Create start stream functionality in "Audio" class
+# TODO: Implement Resample
+# TODO: Add print statements to initialization to log what's happening
 
 class Audio:
     """Class holding PyAudio objects"""
     def __init__ (self):
 
+        self.buffer = queue.Queue() # Initiate queue to put audio segments in
+        self.block_size = int(INPUTRATE / float(BLOCKS_PER_SECOND)) # Determine size of audio blocks
+        self.frame_duration_ms = 1000 * int(self.block_size / INPUTRATE) # Duration of one block of audio in ms
+
         # Initiate PyAudio
         self.pa = pyaudio.PyAudio() 
 
-        # Define Callback function (Constant Data Transfer)
-        # TODO: Multithreading Implementation
-        def callback(in_data, frame_count, time_info, status):
-            return(in_data, pyaudio.paContinue)
+        # Callback function for PyAudio (whenever called, add data to queue)
+        def thread_callback(in_data, frame_count, time_info, status):
+            self.buffer.put(in_data)
+            return(None, pyaudio.paContinue)
 
         # Open Stream
         self.stream = self.pa.open(
@@ -30,7 +38,8 @@ class Audio:
             channels = CHANNELS,
             rate = INPUTRATE,
             input = True, # Specify as input
-            stream_callback = callback
+            frames_per_buffer= self.block_size,
+            stream_callback = thread_callback
         )
         self.stream.start_stream()
 
@@ -39,9 +48,68 @@ class Audio:
         self.stream.stop_stream()
         self.stream.close()
         self.pa.terminate()
+    
+    def read(self):
+        """Return a block of audio data from queue"""
+        return self.buffer.get()
 
-class VoiceActivityDetector:
+class VoiceActivityDetector(Audio):
     """Class holding WebRTC VAD implementation, detecting voice activity and feeding it to DeepSpeech"""
 
     def __init__(self, aggressiveness):
         self.vad = webrtcvad.Vad(aggressiveness)
+        super().__init__()
+
+    def frame_generator(self):
+        """Generator that yields audio frames from microphone"""
+        while True:
+            yield self.read()
+
+    def vad_collector(self, padding_ms = 300, ratio = 0.75):
+        """
+        Generator that yields series of audio segments with "None" between them by iterating a "sliding window" over audio.
+
+        padding_ms is the window observed around the audio (Default: 300ms)
+
+        ratio is the ratio of audio frames required in the padding zone for the VAD to trigger (Default: 75%)
+        """
+        frames = self.frame_generator() # Extracting frames from audio
+        num_padding_frames = int(padding_ms / self.frame_duration_ms) # Number of padding frames based on ms given (default: 6)
+        ring_buffer = collections.deque(maxlen=num_padding_frames) # List-like sequence with fast access at endpoints, used for sliding window
+
+        # Generator has two states: TRIGGERED and NOT TRIGGERED, initiallly we set it to not triggered.
+        triggered = False
+
+        for frame in frames:
+
+            # Extra line of code contained in model example, discard small samples?
+            #if len(frame) < 640:
+            #    return
+
+            is_speech = self.vad.is_speech(frame, INPUTRATE) # check whether given frame is a speech frame or not
+
+            if not triggered: # NOT TRIGGERED State
+                ring_buffer.append((frame, is_speech)) # add frame to queue as (data,False)
+                num_voiced = len([f for f, speech in ring_buffer if speech]) # sum up number of speech frames in queue
+
+                # If VAD in NOT TRIGGERED state and more than "ratio"% of frames in ring buffer are voiced, enter TRIGGERED state
+                if num_voiced > ratio * ring_buffer.maxlen:
+                    triggered = True
+
+                    # Start yielding audio by yielding the frames already in queue
+                    for f, s in ring_buffer:
+                        yield f
+                    ring_buffer.clear()
+
+            else: # TRIGGERED State
+                yield frame
+                ring_buffer.append((frame, is_speech)) # add frame to queue as (data,True)
+                num_unvoiced = len([f for f, speech in ring_buffer if not speech]) # sum up number of non-speech frames in queue
+
+                # If VAD in TRIGGERED state and more than "ratio"% of frames in ring buffer are non-voiced, enter NOT TRIGGERED state
+                if num_unvoiced > ratio * ring_buffer.maxlen:
+                    triggered = False
+
+                    # Stop yielding audio
+                    yield None
+                    ring_buffer.clear()
